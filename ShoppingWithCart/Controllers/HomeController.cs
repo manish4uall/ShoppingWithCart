62c366a02e321395ff7a2a8c0eeadbeee0d49fb0
Manish Verma <mr.manish4uall@gmail.com>
2018-12-20 22:01:00 +0530

    Updated

--- ShoppingWithCart/Controllers/HomeController.cs
+++ ShoppingWithCart/Controllers/HomeController.cs
@@ -10,21 +10,69 @@ namespace ShoppingWithCart.Controllers
     {
         public ActionResult Index()
         {
+            using(Entities dbObj = new Entities())
+            {
+                var results = dbObj.tblProductDtls.ToList();
+                return View(results);
+            }
+            
+        }
+        public ActionResult Cart()
+        {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult PlaceOrder()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public JsonResult AddToCart(int id)
         {
-            ViewBag.Message = "Your contact page.";
+            List<int> ProductIds = new List<int>();
+            bool productAlreadyExists = false;
 
-            return View();
+            if ( Session["Cart"] == null)
+            {
+                ProductIds.Add(id);
+                Session["Cart"] = ProductIds;
+            }
+            else
+            {
+                ProductIds = (List<int>)Session["Cart"];
+                if(ProductIds.Any(x => x == id)){
+                    productAlreadyExists = true;
+                }
+                else{
+                    ProductIds.Add(id);
+                    Session["Cart"] = ProductIds;
+                }
+                
+            }
+            return Json(new {cartItemsCount = ProductIds.Count, cartItems = ProductIds, productStatus = productAlreadyExists },JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GetCartItems()
+        {
+            List<int> ids = new List<int>();
+            if (Session["Cart"] != null)
+            {
+                ids = (List<int>)Session["Cart"];
+                return Json(new { cartItemsCount = ids.Count, cartItems = ids });
+            }
+            else
+            {
+                return Json(new { cartItemsCount = 0, cartItems = ids },JsonRequestBehavior.AllowGet);
+            }
+            
+        }
+
+        [HttpGet]
+        public ActionResult Test()
+        {
+            return Json(new { msg = "test" }, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ ShoppingWithCart/mdl/MyJavaScript.js
@@ -0,0 +1,63 @@
+﻿
+GetCartItems();
+
+
+function GetCartItems() {
+    $.ajax({
+        url: "/Home/GetCartItems",
+        data: JSON.stringify("1"),
+        type: "POST",
+        dataType: "json",
+        contentType: "application/json",
+
+        success: function (data) {
+            console.log("GetCartItems success =", data);
+            SetCartItemCount(data.cartItemsCount);
+        },
+        error: function (data) {
+            console.log("GetCartItems error =", data);
+        }
+    });
+}
+
+
+function AddToCart(id) {
+    console.log("AddToCart ProductId =", id);
+    var obj = { id: id };
+    $.ajax({
+        url: "/Home/AddToCart",
+        data: JSON.stringify(obj),
+        type: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        success: function (data) {
+            console.log("AddToCart success =", data);
+            console.log("AddToCart productExistenceStatus =", data.productStatus);
+            SetCartItemCount(data.cartItemsCount);
+
+            if (data.productStatus == true) {
+                NotifyProductAlreadyExists();
+            }
+        },
+        error: function (data) {
+            console.log("AddToCart error =", data);
+        }
+    });
+}
+
+
+function SetCartItemCount(count) {
+    console.log("SetCartItemCount count =", count);
+    $("#cartItemCount").html('');
+    $("#cartItemCount").append(count);
+}
+
+function NotifyProductAlreadyExists() {
+    var notification = document.querySelector('.mdl-js-snackbar');
+    notification.MaterialSnackbar.showSnackbar(
+        {
+            message: 'Product is Already in the Cart'
+        }
+    );
+}
+ 
